85ae6d2295aa557d7c7f5d96bccad4ada5d0757e
TsimpDim <dimitristsimpitas@yahoo.gr>
2017-11-27 11:36:40 +0200

    Removed unnecessary comments

--- 3RStore/3RStore.py
+++ 3RStore/3RStore.py
@@ -4,9 +4,8 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 
 
 app = Flask(__name__)
-app.config.from_object(__name__) # load config from this file , flaskr.py
+app.config.from_object(__name__)
 
-# Load default config and override config from an environment variable
 
 @app.route('/')
 def index():
