b217a4d364356a882188daf0c03184b847008645
TsimpDim <dimitristsimpitas@gmail.com>
2018-08-05 11:17:27 +0300

    Updated .gitignore

--- _3RStore/classes.py
+++ _3RStore/classes.py
@@ -1,15 +1,14 @@
-from typing import List
 from anytree import NodeMixin
 
 class BaseResource():
 
-    def __init__(self, title:str, link:str, tags:List):
+    def __init__(self, title:str, link:str, tags:list):
         self.title = title
         self.link = link
         self.tags = tags
 
 class MixinResource(BaseResource, NodeMixin):
-    def __init__(self, title:str, link:str, tags:List, name:str, length:int, width:int, parent=None):
+    def __init__(self, title:str, link:str, tags:list, name:str, length:int, width:int, parent=None):
         super().__init__(title, link, tags)
         self.name = name
         self.length = length
