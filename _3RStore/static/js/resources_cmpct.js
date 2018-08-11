4223f8c776737fdbcef6cc48dd1c559c8995610e
TsimpDim <dimitristsimpitas@gmail.com>
2018-08-11 13:13:10 +0300

    Added filtered search in compact view

--- _3RStore/static/js/resources_cmpct.js
+++ _3RStore/static/js/resources_cmpct.js
@@ -14,33 +14,44 @@ function containsOtherArray(thisArray, otherArray){
 }
 
 function initiateSearch(){
-    let input = $('#search_input').val().toLowerCase().split(','); // Array with requested tags
+    let inputTags = $('#search_input').val().toLowerCase().split(','); // Array with requested tags
     let resources = $('.re_card'); // Array with resources
+    let filters;
+    let useFilters = false;
 
-    if(input.length == 1 && input[0] == ""){ // If no tags have been entered
+    if($('#search_input').val().indexOf('-') > -1){
+        useFilters = true;
+        filters  = $('#search_input').val().toLowerCase().split('-')[1].split(',');
+        inputTags = $('#search_input').val().toLowerCase().split('-')[0].trim().split(',');
+    }
+
+    if(inputTags.length == 1 && inputTags[0] == ""){ // If no tags have been entered
         $("#cards_cont").show(); // Show all the resources
         $('.re_card').show();
     }else{
         resources.each(function(){
             
             let tags = $(this).find('.re_tags'); 
-            let tag_array = [];
+            let resTagArray = [];
             let cur_r_title = $(this).find('h4 > a').text().toLowerCase();
 
             // Get a cleaned up array of the given (by the user) tags
             tags.each(function(i){
                 let curr_tag = $(this).text().trim();
                 if(curr_tag != null || curr_tag.length > 0)
-                    tag_array.push(curr_tag);
+                    resTagArray.push(curr_tag);
             });
 
             // Show the resource if the tags match or if the title includes the input
-            if(containsOtherArray(input, tag_array) || cur_r_title.includes(input[0])){
-                $(this).show();
-                $("#cards_cont").show();
+            if(containsOtherArray(inputTags, resTagArray) || cur_r_title.includes(inputTags[0])){
+                if(useFilters && !checkFilters(resTagArray, filters))
+                $(this).hide();
+                else{
+                    $(this).show();
+                    $("#cards_cont").show();
+                }
             }else
                 $(this).hide();
-
         });
 
         if($("#cards_cont").children(':visible').length == 0)
@@ -73,4 +84,16 @@ function showNote(){
     let text = master.find('.card-footer').text(); // Get the text
     text = text.replace(/<\/br\>/g, '\n'); // Properly show the text
     alert(text);
+}
+
+function checkFilters(tags, filter){
+
+    let include = true;
+
+    tags.forEach(function(i){
+        if(filter.includes(i))
+            include = false;
+    });
+
+    return include;
 }
