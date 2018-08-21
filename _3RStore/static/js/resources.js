1c0780e863f699b8ad5d30319018ccfc30133188
TsimpDim <dimitristsimpitas@gmail.com>
2018-08-21 09:43:57 +0300

    Added copy-link functionality in both views

--- _3RStore/static/js/resources.js
+++ _3RStore/static/js/resources.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('#search_input').on('input', initiateSearch);
     $('input').on('input', checkDisabled);
+    $('.copy_link').on('click', copyLink);
 });
 
 
@@ -81,4 +82,18 @@ function checkFilters(tags, filter){
     });
 
     return include;
+}
+
+function copyLink(){
+    let link = $(this).data('link');
+
+    let el = document.createElement('input');
+    el.value = link;
+
+    document.body.appendChild(el);
+    el.select();
+
+    document.execCommand('copy');
+
+    document.body.removeChild(el);
 }
